39b5661b609a7ad43c883dfcb2ce21994c9a3c20
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2024-02-08 10:25:36 -0300

    first commit

---
+++ src/test/java/br/com/mmmsieto/javavirtualthreads/JavaVirtualThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mmmsieto.javavirtualthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVirtualThreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
